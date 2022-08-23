5b3406a9a3bca44a285e49040ad8a14b11408d4c
lass026q <lass026q@stud.kea.dk>
2022-08-23 09:19:46 +0200

    asd

---
+++ src/main/java/com/example/githubactiondemo/GitHubActionDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.githubactiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GitHubActionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactiondemo/GitHubActionDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.githubactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
